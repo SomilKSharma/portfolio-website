49fa15ee145f712a72bce7601b85e98e7c0431cc
Somil Sharma <somil81@icloud.com>
2023-08-28 22:40:57 +0530

    Added comments

--- script.js
+++ script.js
@@ -1,17 +1,23 @@
+// Function to handle the media query change
 function handleMediaQuery(mediaQuery) {
-    const element = document.querySelector('.testimonial_cards');
-    const element_2 = document.querySelector('.testimonial_cards_bg');
+    const element = document.querySelector('.testimonial_cards'); // Select the first element
+    const element_2 = document.querySelector('.testimonial_cards_bg'); // Select the second element
   
     if (mediaQuery.matches) {
-      element.classList.add('hidden');
-      element_2.classList.remove('hidden');
+      element.classList.add('hidden'); // Add 'hidden' class to the first element
+      element_2.classList.remove('hidden'); // Remove 'hidden' class from the second element
     } else {
-      element.classList.remove('hidden');
-      element_2.classList.add('hidden');
+      element.classList.remove('hidden'); // Remove 'hidden' class from the first element
+      element_2.classList.add('hidden'); // Add 'hidden' class to the second element
     }
   }
   
+  // Create a media query to match screen width
   const mediaQuery = window.matchMedia('(min-width: 769px)');
-  handleMediaQuery(mediaQuery); // Initial check
   
-  mediaQuery.addEventListener('change', handleMediaQuery); // Listen for changes
+  // Initial check when the page loads
+  handleMediaQuery(mediaQuery);
+  
+  // Listen for changes in the media query
+  mediaQuery.addEventListener('change', handleMediaQuery);
+  
